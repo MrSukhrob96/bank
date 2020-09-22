a66896a5a5542106dc32e0d00c3825b2e5744ff3
= <=>
2020-09-22 09:43:55 +0300

    test

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -29,3 +29,27 @@ func ExampleAvg() {
 
 	//Output: 5200
 }
+
+func ExampleTotalInCategory() {
+	payments := []types.Payment{
+		{
+			ID:       2,
+			Amount:   53_00,
+			Category: "Cafe",
+		},
+		{
+			ID:       1,
+			Amount:   51_00,
+			Category: "Cafe",
+		},
+		{
+			ID:       3,
+			Amount:   52_00,
+			Category: "Restaurant",
+		},
+	}
+
+	fmt.Println(TotalInCategory(payments, "Cafe"))
+
+	//Output: 5200
+}
--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -15,3 +15,16 @@ func Avg(pyments []types.Payment) types.Money {
 	avg := summ / types.Money(len(pyments))
 	return avg
 }
+
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	summ := types.Money(0)
+	i := types.Money(0)
+
+	for _, pyment := range payments {
+		if pyment.Category == category {
+			summ = summ + pyment.Amount
+			i++
+		}
+	}
+	return summ / i
+}
